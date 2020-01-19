bdb4f66d1c951a56556f3b54b724d96a473fbe6a
milady28 <53426214+milady28@users.noreply.github.com>
2020-01-20 09:07:02 +1000

    Автоматизация (6)

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task("html", function () {
 });
 
 gulp.task("images", function () {
-  return gulp.src("source/img/**/*.{png, jpg, svg}")
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
   .pipe(imagemin([
     imagemin.optipng({optimizationLevel: 3}),
     imagemin.jpegtran({progressive: true}),
@@ -60,7 +60,7 @@ gulp.task("images", function () {
 });
 
 gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png, jpg}")
+  return gulp.src("source/img/**/*.{png,jpg}")
   .pipe(webp({quality: 90}))
   .pipe(gulp.dest("source/img"));
 });
